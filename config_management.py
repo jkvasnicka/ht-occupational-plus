5bced004e17d9eac9e93d533dd4dc33b8c2bfa8a
Jacob Kvasnicka <jkvasnicka@users.noreply.github.com>
2024-08-27 15:58:42 -0400

    Initialize config management with UnifiedConfiguration

---
+++ config_management.py
@@ -0,0 +1,51 @@
+'''
+This module contains the `UnifiedConfiguration` class, which centralizes the 
+management and access of configuration settings related to different categories,
+such as paths, models, and plotting. By unifying the configuration into a 
+single object, it allows for a more streamlined access to settings throughout 
+the code.
+
+Example
+-------
+config_file = 'config.json'
+config = UnifiedConfiguration(config_file)
+model_settings = config.model
+'''
+
+import json
+
+#region: UnifiedConfiguration.__init__
+class UnifiedConfiguration:
+    '''
+    This class provides a unified interface to access configuration settings 
+    related to different categories such as paths, models, and plotting. The 
+    configuration files for each category are loaded and made accessible as 
+    attributes.
+    '''
+    def __init__(self, config_file=None, encoding=None):
+        '''
+        Initialize the UnifiedConfiguration object.
+
+        Parameters
+        ----------
+        config_file : str, optional
+            Path to the JSON file mapping categories to configuration file 
+            paths. By default, will look for 'config.json' in the working 
+            directory.
+        encoding : str, optional
+            Default is 'utf-8'.
+        '''
+        # Set defaults
+        if config_file is None:
+            config_file = 'config.json'  
+        if encoding is None:
+            encoding = 'utf-8'
+
+        with open(config_file, 'r', encoding=encoding) as mapping_file:
+            config_files_dict = json.load(mapping_file)
+
+        # Load each file into its category
+        for category, file_path in config_files_dict.items():
+            with open(file_path, 'r', encoding=encoding) as config_file:
+                setattr(self, category, json.load(config_file))
+#endregion
--- tests/test_cehd.py
+++ tests/test_cehd.py
@@ -11,46 +11,31 @@ as `Categorical`.
 import pandas as pd 
 import pytest 
 
+from config_management import UnifiedConfiguration
 from raw_processing import cehd_cleaning
 
-# TODO: Move to config files
-path_settings = {
-    'cehd_file' : 'Input/Processed/osha_data.csv',
-    'qualif_conv_file' : 'Input/Raw/OSHA/CEHD/CEHD1984_2018/cleaning scripts/Conversion tables/qualif_new_2020.csv',
-    'unit_conv_file' : 'Input/Raw/OSHA/CEHD/CEHD1984_2018/cleaning scripts/Conversion tables/unit_conv_2020.csv',
-    'it_directory' : 'Input/Raw/OSHA/CEHD/CEHD1984_2018/cleaning scripts/Conversion tables IT',
-    'cehd_log_file' : 'cehd_log_file.json'
-} 
-
-cehd_settings = {
-    'dtype' : {
-        'CITY': 'str',
-        'ESTABLISHMENT_NAME': 'str',
-        'FIELD_NUMBER': 'str',
-        'IMIS_SUBSTANCE_CODE' : 'str',
-        'INSPECTION_NUMBER': 'str',
-        'INSTRUMENT_TYPE': 'str',
-        'NAICS_CODE': 'str',
-        'QUALIFIER': 'str',
-        'SAMPLING_NUMBER': 'str',
-        'SUBSTANCE': 'str',
-        'UNIT_OF_MEASUREMENT': 'str',
-        'ZIP_CODE': 'str'
-    }
-}
-
+#region: config fixture
 @pytest.fixture
-def raw_exposure_data():
+def config():
+    '''
+    '''
+    return UnifiedConfiguration()
+#endregion
+
+#region: raw_exposure_data fixture
+@pytest.fixture
+def raw_exposure_data(config):
     '''
     '''
     return pd.read_csv(
-        path_settings['cehd_file'], 
+        config.path['cehd_file'], 
         index_col=0,
-        dtype=cehd_settings['dtype']
+        dtype=config.cehd['dtype']
     )
+#endregion
 
 #region: test_cehd_cleaning
-def test_cehd_cleaning(raw_exposure_data):
+def test_cehd_cleaning(raw_exposure_data, config):
     '''
     Use `pd.testing.assert_frame_equal` to compare the cleaned DataFrame with 
     the expected DataFrame
@@ -66,7 +51,7 @@ def test_cehd_cleaning(raw_exposure_data):
 
     cehd_data = cehd_cleaning.clean_chemical_exposure_health_data(
         raw_exposure_data, 
-        path_settings
+        config.path
         )
     
     pd.testing.assert_frame_equal(cehd_data, expected_data, check_names=False)
