a9566d552b83fecfcdc2b8ac61ea6f796db84d18
Jacob Kvasnicka <jkvasnicka@users.noreply.github.com>
2024-11-11 10:34:48 -0500

    Read and join features: OPERA & NAICS

--- data_management.py
+++ data_management.py
@@ -11,13 +11,23 @@ def read_target(target_dir, naics_level):
     return pd.read_csv(target_file, index_col=[0, 1]).squeeze()
 #endregion
 
+#region: read_features
+def read_features(opera_features_file, y=None):
+    '''
+    '''
+    X_opera = pd.read_parquet(opera_features_file)
+    X_naics = naics_features_from_target(y)
+    return X_naics.join(X_opera, on='DTXSID', how='inner')
+#endregion
+
 #region: naics_features_from_target
-def naics_features_from_target(y, naics_code_col):
+def naics_features_from_target(y):
     '''
     Prepares one-hot-encoded NAICS codes as features.
 
     The features are derived from the target variable's index.
     '''
+    naics_code_col = y.index.names[-1]
     X = pd.get_dummies(y.reset_index()[naics_code_col])
     return X.set_index(y.index)
 #endregion
