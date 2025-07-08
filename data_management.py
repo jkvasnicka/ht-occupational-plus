7c96e80b12c7547916f146ad48c39caaab3d7c0e
Jacob Kvasnicka <jkvasnicka@users.noreply.github.com>
2025-07-08 11:17:32 -0500

    Update docstrings in data_management.py

--- data_management.py
+++ data_management.py
@@ -1,7 +1,10 @@
 '''
+Data I/O utilities for features and target variables.
+
+Provides functions to load and preprocess feature dataframes and target
+series from disk.
 '''
 
-import os
 import pandas as pd
 import numpy as np 
 
@@ -13,6 +16,25 @@ def read_features_and_target(
         log10_features=None
         ):
     '''
+    Load feature matrix and target series, align on index.
+
+    Parameters
+    ----------
+    features_file : str
+        Path to features Parquet file.
+    target_file : str
+        Path to target CSV file (MultiIndex).
+    feature_columns : list of str, optional
+        Subset of columns to select from the features DataFrame.
+    log10_features : list of str, optional
+        Columns to log₁₀-transform after selection.
+
+    Returns
+    -------
+    X : pandas.DataFrame
+        Feature matrix aligned to y.
+    y : pandas.Series
+        Target series aligned to X.
     '''
     X = read_features(
         features_file, 
@@ -25,10 +47,10 @@ def read_features_and_target(
     return X.align(y, join='inner', axis=0)
 #endregion
 
-# TODO: Add parameter validation, e.g., 'log10_features' in 'feature_columns'
 #region: read_features
 def read_features(features_file, feature_columns=None, log10_features=None):
     '''
+    Read and preprocess feature DataFrame.
     '''
     if log10_features is None:
         log10_features = []
@@ -47,38 +69,7 @@ def read_features(features_file, feature_columns=None, log10_features=None):
 #region: read_target
 def read_target(target_file):
     '''
+    Read target series from CSV with MultiIndex (DTXSID, naics_id).
     '''
     return pd.read_csv(target_file, index_col=[0, 1]).squeeze()
-#endregion
-
-# NOTE: This may be obsolete if only one target ends up being used
-#region: read_targets
-def read_targets(root_dir):
-    '''
-    Traverse a directory tree and read target data from CSV files.
-
-    Returns
-    -------
-    dict
-        A dictionary where keys represent the relative path to each file:
-        - Simple strings for files in the root directory.
-        - Tuples of directory names and file name (without extension) for 
-          nested files.
-        Values are the processed target data from each CSV file.
-    '''
-    data_for_name = {}  # initialize
-    for dirpath, _, filenames in os.walk(root_dir):
-        for name in filenames:
-            if name.endswith('.csv'):
-                relative_path = os.path.relpath(dirpath, root_dir)
-                if relative_path == '.':  # root
-                    path_parts = ()
-                else:
-                    path_parts = tuple(relative_path.split(os.sep))
-                k = os.path.splitext(name)[0]  # file name without extension
-                if path_parts:
-                    k = tuple(path_parts + (k,))
-                target_file = os.path.join(dirpath, name)
-                data_for_name[k] = read_target(target_file)
-    return data_for_name
 #endregion
